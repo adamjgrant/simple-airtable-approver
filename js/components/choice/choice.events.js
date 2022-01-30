46d2b78e6cd50b2d05b0fbe8d5d576379f1bb533
Adam Grant <adam_grant@apple.com>
2022-01-30 13:45:39 -0800

    Permutations work.

---
+++ js/components/choice/choice.events.js
@@ -0,0 +1,5 @@
+m.choice.events(_$ => {
+    _$("#choice-lol").addEventListener("click", _$.act.permute_lol);
+    _$("#choice-nice").addEventListener("click", _$.act.permute_nice);
+    _$("#choice-agreed").addEventListener("click", _$.act.permute_agreed);
+})
