67aaf872abadf9b41075cfc2585f0b08d8e7aa10
Adam Grant <adam_grant@apple.com>
2022-01-31 13:12:45 -0800

    Showing scores at the top

--- js/components/top_nav/top_nav.actions.js
+++ js/components/top_nav/top_nav.actions.js
@@ -1,1 +1,30 @@
-m.top_nav.act({});
+m.top_nav.act({
+    set_scores: function set_scores(_$, args) {
+        _$.act.get_scores().then(scores => {
+            _$("#score-rejected").innerText = scores.rejected;
+            _$("#score-approved").innerText = scores.approved;
+            _$("#score-in-review").innerText = scores.in_review;
+        });
+    },
+
+    priv: {
+        get_scores(_$, args = {}) {
+            return new Promise((resolve, reject) => {
+                m.card.act.airtable_base()('🙂 Accounts').find('rec1IPb2uSQz3GKrf', function(err, record) {
+                    if (err) {
+                        if (args.ecb) args.ecb();
+                        m.status_indicator.act.set_status_red();
+                        return console.error(err);
+                    }
+                    if (args.cb) args.cb();
+                    const scores = {
+                        rejected: record.get("# Rejected"),
+                        in_review: record.get("# In Review"),
+                        approved: record.get("# Approved")
+                    }
+                    resolve(scores);
+                });
+            });
+        }
+    }
+});
--- js/components/top_nav/top_nav.events.js
+++ js/components/top_nav/top_nav.events.js
@@ -1,4 +1,7 @@
 m.top_nav.events((_$) => {
     _$("#timeline").addEventListener("click", m.viewport.act.show_timeline);
     _$("#tinder").addEventListener("click", m.viewport.act.show_tinder);
+
+    _$.act.set_scores();
+    setTimeout(_$.act.set_scores, 1500);
 });
