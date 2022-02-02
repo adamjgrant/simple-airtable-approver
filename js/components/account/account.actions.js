47b85b2d09f33db9803a69d93fa15177b1ad48a2
Adam Grant <adam_grant@apple.com>
2022-02-02 11:22:16 -0800

    Adding some notes about what I want to do

--- js/components/account/account.actions.js
+++ js/components/account/account.actions.js
@@ -1,5 +1,31 @@
+m.account.accounts = [];
+
+m.account.Account = class {
+    constructor(handle, airtable_record_id) {
+        this.handle = handle;
+        this.airtable_record_id = airtable_record_id;
+        this.scores = {
+            rejected: 0,
+            in_review: 0,
+            approved: 0
+        }
+    }
+
+    get raw_handle() {
+        // TODO: Use a regex instead to get everything between the "@" and the next space.
+        return this.handle.replace("Permutations", "").replace("@", "").trim();
+    }
+
+    update_score() {
+        // TODO: Just set the in-memory data. The remote call should happen elsewhere
+        //       and across all accounts.
+    }
+}
+
 m.account.acts({
     set_scores: function set_scores(_$, args) {
+        // TODO: This should only update the UI and shows summed-up scores for the
+        //       filtered accounts.
         _$.act.get_scores().then(scores => {
             _$("#score-rejected").innerText = String(scores.rejected).padStart(3, '0');
             _$("#score-in-review").innerText = String(scores.in_review).padStart(3, '0');
@@ -7,9 +33,19 @@ m.account.acts({
         });
     },
 
+    get_accounts_available(_$, args) {
+        // TODO: Populate m.account.accounts with m.account.Account objects
+    },
+
+    set_account_filter(_$, args) {
+        // TODO: Set m.account.account_filter to an array with the m.account.Account
+        //       objects that should be shown.
+    },
+
     priv: {
         get_scores(_$, args = {}) {
             return new Promise((resolve, reject) => {
+                // TODO: Make this work with any account
                 m.card.act.airtable_base()('🙂 Accounts').find('rec1IPb2uSQz3GKrf', function(err, record) {
                     if (err) {
                         if (args.ecb) args.ecb();
