e93d07bd3066d6135e632581df84a8ee10d3309b
Adam Grant <hello@adamgrant.me>
2022-02-06 10:03:38 -0800

    Badge formatting

--- js/components/metadata/metadata.actions.js
+++ js/components/metadata/metadata.actions.js
@@ -13,6 +13,17 @@ m.metadata.act({
 
     set_badge(_$, args) {
         if (m.viewport.current_view === "tinder") _$.act.show_badge();
-        _$("#badge").innerHTML = args.text;
+        const formatted_text = _$.act.format_badge(args);
+        _$("#badge").innerHTML = formatted_text;
+    },
+
+    priv: {
+        format_badge(_$, args) {
+            let number_parsed = args.text.match(/(\d+\.\d+)/);
+            let number = number_parsed ? number_parsed[0] : args.text;
+            number = parseFloat(number.substr(0, 6)) * 100;
+            if (number == 0) number = "---";
+            return args.text.replace(/(\d+\.\d+)/, `${number}%`);
+        }
     }
 })
