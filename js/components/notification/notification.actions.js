c18ea7d9d9d06044fd34ecf3ccc721e55c0ca80c
Adam Grant <hello@adamgrant.me>
2022-08-02 18:04:49 -0700

    Notifications work now.

--- js/components/notification/notification.actions.js
+++ js/components/notification/notification.actions.js
@@ -2,14 +2,12 @@ m.notification.acts({
     show_notification(_$, args) {
         let notification_element = document.createElement("div");
         notification_element.className = "notification";
-        notification.innerHTML = `
-          <div class="notification">
+        notification_element.innerHTML = `
             <span class="icon">${args.icon || ""}</span>
             <h1>${args.message}</h1>
-          </div>
         `;
         _$.me().appendChild(notification_element);
-        setTimeout(() => { _$.act.hide_notification({ element: notification_element }) }, 2000);
+        setTimeout(() => { _$.act.hide_notification({ element: notification_element }) }, args.delay || 3000);
     },
 
     hide_notification(_$, args) {
