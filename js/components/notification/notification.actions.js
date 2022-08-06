1ac3e457083025ba242ce739bff1a96f18879058
Adam Grant <hello@adamgrant.me>
2022-08-06 12:18:52 -0700

    Better styling for reward animations

--- js/components/notification/notification.actions.js
+++ js/components/notification/notification.actions.js
@@ -3,9 +3,9 @@ m.notification.acts({
         let notification_element = document.createElement("div");
         notification_element.className = "notification";
         notification_element.innerHTML = `
-            <span class="icon">${args.icon || ""}</span>
-            <h1>${args.heading}</h1>
-            <p>${args.message}</p>
+            <p class="icon">${args.icon || ""}</p>
+            <h1 class="heading">${args.heading}</h1>
+            <p class="message">${args.message}</p>
         `;
         _$.me().appendChild(notification_element);
         setTimeout(() => { _$.act.hide_notification({ element: notification_element }) }, args.delay || 3000);
