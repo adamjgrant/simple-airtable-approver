c6c982fead8a61e4c3501a71c991d686fed4f27a
Adam Grant <hello@adamgrant.me>
2022-08-07 10:59:02 -0700

    Hastening resetter to five seconds

--- js/components/odometer/odometer.events.js
+++ js/components/odometer/odometer.events.js
@@ -5,6 +5,6 @@ m.odometer.events(_$ => {
   setInterval(_$.act.update_odometer, C.FIVE_SECONDS_IN_MS);
   // Need to allow for the first "enough time has passed" interval to happen before we do this.
   setTimeout(() => {
-    setInterval(_$.act.reset_interval, C.FIFTEEN_SECONDS_IN_MS);
+    setInterval(_$.act.reset_interval, C.FIVE_SECONDS_IN_MS);
   }, C.ONE_MINUTE_IN_MS);
 });
