ca282f0a4079e3c3dca204e543ad8d56d376348b
Adam Grant <hello@adamgrant.me>
2022-08-07 10:51:14 -0700

    Faster resetting of interval

--- js/components/odometer/odometer.events.js
+++ js/components/odometer/odometer.events.js
@@ -1,5 +1,5 @@
 m.odometer.events(_$ => {
   _$.act.init();
   setInterval(_$.act.update_odometer, 5000);
-  setInterval(_$.act.reset_interval, 60000)
+  setInterval(_$.act.reset_interval, 10000);
 });
